a8cbdc26faab1d6ed31a39bdc4dac46f11409bd0
MatheusFukuda <matheusfukuda@hotmail.com>
2021-06-10 13:23:42 -0300

    teste css 10062021 maugs

--- TorneioFacil/www/index.php
+++ TorneioFacil/www/index.php
@@ -9,23 +9,26 @@
    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
+        <title>
+            Menu
+        </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
 
-<h1>Menu</h1>
-    <a href="./modulos/Select_Adicionar.php">Adicionar</a>
+<body>
+    <div class= "panel">
+        <center>
+            <h1>
+                <a a href= "https://github.com/fernandOlle/PBD-UFPel" class= "test"><img src="pbdprototipo.png" width= auto; height= 220px;></a>
+            </h1>
+        </center>
+    </div>
     <br>
-    <br>
-    <br>
-    <a href="./modulos/Select_Listar.php">Listar</a>
-    <br>
-    <br>
-    <br> 
-    <a href="./modulos/Select_Apagar.php">Apagar</a>
-    <br>
-    <br>
-    <br>
-    <a href="./modulos/Buscas.php">BuscasSQL</a>
+        <center>
+        <a href="./modulos/Select_Adicionar.php" class= "button buttontest">Adicionar</a>
+        <a href="./modulos/Select_Listar.php" class= "button buttontest">Listar</a>
+        <a href="./modulos/Select_Apagar.php" class= "button buttontest">Apagar</a>
+        <a href="./modulos/Buscas.php" class= "button buttontest">BuscasSQL</a>
+        </center>
 </body>
 </html>
