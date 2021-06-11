b487b8c373a02ab62733be2c2bff62ded670f51f
MatheusFukuda <matheusfukuda@hotmail.com>
2021-06-11 03:30:41 -0300

    commit css 11062021 0330 by maugs

--- TorneioFacil/www/index.php
+++ TorneioFacil/www/index.php
@@ -9,6 +9,7 @@
    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="E.png" />
         <title>
             Menu
         </title>
@@ -19,16 +20,18 @@
     <div class= "panel">
         <center>
             <h1>
-                <a a href= "https://github.com/fernandOlle/PBD-UFPel" class= "test"><img src="pbdprototipo.png" width= auto; height= 220px;></a>
+                <a href= "https://github.com/fernandOlle/PBD-UFPel"><img src="pbdlogo.png" class= "seila" width= auto; height= 220px;></a>
             </h1>
         </center>
     </div>
-    <br>
+    <div class= "btpanel"></div>
+        <br>
         <center>
         <a href="./modulos/Select_Adicionar.php" class= "button buttontest">Adicionar</a>
         <a href="./modulos/Select_Listar.php" class= "button buttontest">Listar</a>
         <a href="./modulos/Select_Apagar.php" class= "button buttontest">Apagar</a>
         <a href="./modulos/Buscas.php" class= "button buttontest">BuscasSQL</a>
         </center>
+        <br>
 </body>
 </html>
